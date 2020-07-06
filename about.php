bd0d0fc0363c50edb4678b0ebe99a06cb9572731
Adan Carrillo Hernandez <67882101+AdanCarrillo@users.noreply.github.com>
2020-07-05 21:27:44 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Tienda</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+    <?php include("./layouts/header.php"); ?> 
+
+    <div class="site-section border-bottom" data-aos="fade">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-6">
+            <div class="block-16">
+              <figure>
+                <img src="images/nosotros.jpg" alt="Image placeholder" class="img-fluid rounded">
+                
+
+              </figure>
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+          <div class="col-md-5">
+            
+            
+            <div class="site-section-heading pt-3 mb-4">
+              <h2 class="text-black">Nosotros</h2>
+            </div>
+            <p>Somos una organizacion que contribuye al bienestar de nuestra sociedad brindando fuentes de empleo, ofreciendo soluciones en licores para el publico e industria en general.</p>
+            <p>Impulsamos la venta de bebidas alcoholicas con un alto sentido de responsabilidad, comercializando solo productos 100% genuinos, adquiridos directamente del fabricante, para ofrecerte un producto de la mas alta calidad a un precio competitivo. </p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <?php include("./layouts/footer.php"); ?> 
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Bar Adan</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700"> 
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+    <?php include("./layouts/header.php"); ?> 
+
+    <div class="site-section">
+      <div class="container">
+
+        <div class="row mb-5">
+          <div class="col-md-9 order-2">
+
+            <div class="row">
+              <div class="col-md-12 mb-5">
+                <div class="float-md-left mb-4"><h2 class="text-black h5">Nuestros Productos</h2></div>
+                <div class="d-flex">
+                 
+
+                  <div class="btn-group">
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="row mb-5">
+              <?php
+              include('./php/conexion.php');
+              $limite = 5; //productos por pagina
+              $totalQuery = $conexion->query('select count(*) from productos')or die($conexion->error);
+              $totalProductos = mysqli_fetch_row($totalQuery);
+              $totalBotones = round($totalProductos[0] /$limite);
+              if(isset($_GET['limite'])){
+                $resultado=$conexion->query("select * from productos where inventario > 0 order by id DESC limit ".$_GET['limite'].",".$limite)or die($conexion->error);
+
+              }else{
+                $resultado=$conexion->query("select * from productos where inventario > 0 order by id DESC limit ".$limite)or die($conexion->error);
+
+              }
+              
+              while($fila = mysqli_fetch_array($resultado)){
+             
+              ?>
+              <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
+                <div class="block-4 text-center border">
+                  <figure class="block-4-image">
+                    <a href="shop-single.php?id=<?php echo $fila['id'] ; ?>">
+                      <img src="images/<?php echo $fila['imagen'] ; ?>" alt="<?php echo $fila['nombre'];?> "class="img-fluid"></a>
+                  </figure>
+                  <div class="block-4-text p-4">
+                    <h3><a href="shop-single.php?id=<?php echo $fila['id'] ; ?>"><?php echo $fila['nombre'] ; ?></a></h3>
+                    <p class="mb-0"><?php echo $fila['descripcion'] ; ?></p>
+                    <p class="text-primary font-weight-bold">$<?php echo $fila['precio'] ; ?></p>
+                  </div>
+                </div>
+              </div>
+              <?php } ?>
+            
+
+
+            </div>
+            <div class="row" data-aos="fade-up">
+              <div class="col-md-12 text-center">
+                <div class="site-block-27">
+                <ul>
+                     <?php
+                     if(isset($_GET['limite'])){
+                       if($_GET['limite']>0){
+                         echo '<li><a href="index.php?limite='.($_GET['limite']-5).'">&lt;</a></li>';
+                       }
+                     }
+                       for($k=0;$k<$totalBotones;$k++){
+                         echo '<li><a href="index.php?limite='.($k*5).'">'.($k+1).'</a></li>';
+                       }
+                       if(isset($_GET['limite'])){
+                         if($_GET['limite']+5 < $totalBotones*5){
+                           echo ' <li><a href="index.php?limite='.($_GET['limite']+5).'">&gt;</a></li>';
+
+                         }
+                       }else{
+                         echo ' <li><a href="index.php?limite=5">&gt;</a></li>';
+                       }
+                      ?>
+                   
+
+                  </ul>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-3 order-1 mb-5 mb-md-0">
+            <div class="border p-4 rounded mb-4">
+              <h3 class="mb-3 h6 text-uppercase text-black d-block">Categorias</h3>
+              <ul class="list-unstyled mb-0">
+                <li class="mb-1"><a href="#" class="d-flex"><span>Tequila</span>
+                <li class="mb-1"><a href="#" class="d-flex"><span>Whisky</span>
+                <li class="mb-1"><a href="#" class="d-flex"><span>Ron</span>
+              </ul>
+            </div>
+            </div>        
+        </div>        
+      </div>
+    </div>
+    <?php include("./layouts/footer.php"); ?>     
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
