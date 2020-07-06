79b67b6a0628678f051eb8cc098b9c641ef14239
Adan Carrillo Hernandez <67882101+AdanCarrillo@users.noreply.github.com>
2020-07-05 21:39:05 -0500

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,37 @@
+<footer class="site-footer border-top">
+      <div class="container">
+        <div class="row">                                     
+          <div class="col-md-6 col-lg-3">
+            <div class="block-5 mb-5">
+              <h3 class="footer-heading mb-4">Informacion de Contacto</h3>
+              <ul class="list-unstyled">
+                <li class="address">Tepexoyuca Ocoyoacac, Estado de Mexico</li>
+                <li class="phone"><a href="tel://23923929210">+2 392 3929 210</a></li>
+                <li class="email">arch508.ac@gmail.com</li>
+              </ul>
+            </div>
+
+            <div class="block-7">
+              <form action="#" method="post">
+                <label for="email_subscribe" class="footer-heading">Suscribete</label>
+                <div class="form-group">
+                  <input type="text" class="form-control py-4" id="email_subscribe" placeholder="Email">
+                  <p><a href="index.php" class="btn btn-sm btn-primary">Enviar</a></p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>         
+            Copyright &copy;
+            <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+            <script>document.write(new Date().getFullYear());</script> Todos los derechos reservados | 
+            Adan Carrillo Tiendita <i class="icon-heart" aria-hidden="true"></i>        
+            </p>
+          </div>          
+        </div>
+      </div>
+    </footer>
